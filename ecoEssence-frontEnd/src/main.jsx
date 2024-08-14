e6c35c8ce562a84415269133e52be36ed8e7f161
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-14 23:14:07 +0600

    refactor: Update main.jsx to use Router and RouterProvider for routing

--- ecoEssence-frontEnd/src/main.jsx
+++ ecoEssence-frontEnd/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Router } from './routes/Router/Router.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={Router} />
+  </StrictMode>
 )
