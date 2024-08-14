4c7b6d00a4bae447949564f5891763f094c8d33e
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-15 00:01:59 +0600

    refactor: Update MongoDB connection and add server API version

---
+++ ecoEssence-backEnd/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://eco-essence.web.app",
+      "https://eco-essence.firebaseapp.com/",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+// mongoDB
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wu8kmms.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// ====================================
+//         Mongo DB Connection
+// ====================================
+async function run() {
+  try {
+    // User collection
+    const userCollection = client.db("ecoEssenceDB").collection("users");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Eco Essence server is running");
+});
+
+// listening port
+app.listen(port, () => {
+  console.log("Eco Essence server is listening on port " + port);
+});
