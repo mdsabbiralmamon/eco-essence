682687839285bee12cc906fe6c84d3f1bb623406
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-15 21:31:09 +0600

    feat: Add SignUp and SignIn pages to Router
    This commit adds the SignUp and SignIn pages to the Router component, allowing users to navigate to the /signup and /signin routes respectively.

--- ecoEssence-frontEnd/src/routes/Router/Router.jsx
+++ ecoEssence-frontEnd/src/routes/Router/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../../layout/Root/Root";
 import Home from "../../pages/Home/Home";
+import SignUp from "../../pages/SignUp/SignUp";
+import SignIn from "../../pages/SignIn/SignIn";
 
 export const Router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const Router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            },
+            {
+              path: "/signup",
+              element: <SignUp />,
+            },
+            {
+              path: "/signin",
+              element: <SignIn />,
             }
         ],
     },
