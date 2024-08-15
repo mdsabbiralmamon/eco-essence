c2e0bc5d5488fd8e50178b0379f912c7f69e0554
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-16 03:06:36 +0600

    feat: Add "Products" page and route
    This commit adds the "Products" page component and the corresponding route in the Router.jsx file. The page displays a list of available products, and the route ensures that only authenticated users can access it. This enhances the functionality of the application by providing a dedicated page for viewing and interacting with products.

--- ecoEssence-frontEnd/src/routes/Router/Router.jsx
+++ ecoEssence-frontEnd/src/routes/Router/Router.jsx
@@ -3,6 +3,8 @@ import Root from "../../layout/Root/Root";
 import Home from "../../pages/Home/Home";
 import SignUp from "../../pages/SignUp/SignUp";
 import SignIn from "../../pages/SignIn/SignIn";
+import Products from "../../pages/Products/Products";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 export const Router = createBrowserRouter([
     {
@@ -12,6 +14,10 @@ export const Router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            },
+            {
+                path: "/all-products",
+                element: <PrivateRoute><Products /></PrivateRoute>,
             },
             {
               path: "/signup",
