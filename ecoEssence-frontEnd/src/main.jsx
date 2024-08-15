8bd7e3a0fe023f5d13c2d5e8398036e68371da78
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-15 21:31:48 +0600

    feat: Add AuthProvider component and update main.jsx
    This commit adds the AuthProvider component to handle user authentication and updates the main.jsx file to wrap the RouterProvider with the AuthProvider. This allows for user authentication functionality throughout the application.

--- ecoEssence-frontEnd/src/main.jsx
+++ ecoEssence-frontEnd/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Router } from './routes/Router/Router.jsx'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './context/AuthProvider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Router} />
+    <AuthProvider>
+      <RouterProvider router={Router} />
+    </AuthProvider>
   </StrictMode>
 )
