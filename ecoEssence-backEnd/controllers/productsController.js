bdac0797a92e003f1c28c3265ec649fa4ec8f29b
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-16 12:20:04 +0600

    feat: Improve pagination and filtering for getAllProducts API
    The commit updates the getAllProducts API endpoint in the productsController.js file to include pagination and filtering options. The code changes introduce query parameters for page number, limit, sorting, search, brand, category, minimum price, and maximum price. The endpoint now fetches products based on these parameters, allowing users to view a specific page of products and filter them based on various criteria. This enhances the functionality of the application by providing a more flexible and user-friendly product browsing experience.

--- ecoEssence-backEnd/controllers/productsController.js
+++ ecoEssence-backEnd/controllers/productsController.js
@@ -3,10 +3,52 @@ const client = require("../config/database");
 // Get All Products API
 const getAllProducts = async (req, res) => {
   try {
-    const products = await client.db("ecoEssenceDB").collection("products").find().toArray();
-    res.json(products);
+    const { page = 1, limit = 12, sort = 'newest', search = '', brand = '', category = '', minPrice = 0, maxPrice = 10000 } = req.query;
+
+    // Convert page and limit to numbers
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+
+    // Build query object
+    const query = {
+      name: { $regex: search, $options: 'i' },
+      brand: { $regex: brand, $options: 'i' },
+      category: { $regex: category, $options: 'i' },
+      price: { $gte: parseFloat(minPrice), $lte: parseFloat(maxPrice) }
+    };
+
+    // Define sorting
+    let sortOption;
+    switch (sort) {
+      case 'priceAsc':
+        sortOption = { price: 1 };
+        break;
+      case 'priceDesc':
+        sortOption = { price: -1 };
+        break;
+      case 'newest':
+      default:
+        sortOption = { dateAdded: -1 };
+        break;
+    }
+
+    // Fetch products with pagination
+    const products = await client.db("ecoEssenceDB").collection("products")
+      .find(query)
+      .sort(sortOption)
+      .skip((pageNumber - 1) * limitNumber)
+      .limit(limitNumber)
+      .toArray();
+
+    // Count total products for pagination
+    const totalProducts = await client.db("ecoEssenceDB").collection("products").countDocuments(query);
+
+    res.json({
+      products,
+      totalPages: Math.ceil(totalProducts / limitNumber),
+    });
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch users" });
+    res.status(500).json({ error: "Failed to fetch products" });
   }
 };
 
