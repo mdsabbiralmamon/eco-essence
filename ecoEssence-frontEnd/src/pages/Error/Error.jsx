e9db85363afd59319dd7e230a1f01a87d66d466f
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-16 19:31:06 +0600

    feat: Add Error page component
    The commit adds the Error page component to the ecoEssence front-end. This component is responsible for rendering the "404 page not found" content on the website. It provides a user-friendly error page with a link to go back to the home page. This addition enhances the user experience by handling and displaying errors in a more informative and user-friendly manner.

---
+++ ecoEssence-frontEnd/src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+
+const Error = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <p>404 page not found <br />
+            <a href="/" className="btn btn-primary">Go Home</a></p>
+        </div>
+    );
+};
+
+export default Error;
